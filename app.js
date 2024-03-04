7eed721bd10fd80f2c2b5baf7ec528f49838f597
km539 <71085102+km539@users.noreply.github.com>
2024-03-04 20:49:44 +0900

    add frontend in test.yml

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("/name", (req, res) => {
   res.status(200).send("Hello World");
